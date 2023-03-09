0b89c76c91d44c11824e99e6a7cd3a87d5100986
greatdogsite <42046646+greatdogsite@users.noreply.github.com>
2023-03-09 17:24:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.addEventListener("load", function () {
             alert("Please enter information in all fields.");
             event.preventDefault();
         } else {
-            formSubmission(document, form, pilotName, copilotName, fuelLevel, cargoMass);
+            formSubmission(document, form, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
         }
     });
 
